c34b75743c15f0cae34cdd9e5b9daa066df27807
Rayjan Wilson <rayjwil@amazon.com>
2022-03-11 14:04:59 -0500

    adds .vscode settings

--- bin/cdk-cross-account-secret-sharing.ts
+++ bin/cdk-cross-account-secret-sharing.ts
@@ -4,18 +4,4 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkCrossAccountSecretSharingStack } from '../lib/cdk-cross-account-secret-sharing-stack';
 
 const app = new cdk.App();
-new CdkCrossAccountSecretSharingStack(app, 'CdkCrossAccountSecretSharingStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new CdkCrossAccountSecretSharingStack(app, 'CdkCrossAccountSecretSharingStack', {});
